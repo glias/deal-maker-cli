a6039912a9e3c53911a143b189cb853261f3aa59
Dex Dev <dex-dev@outlook.com>
2020-11-06 20:36:12 +0800

    refactor: refactor and test signAndSendTransaction

--- src/utils/request.spec.ts
+++ src/utils/request.spec.ts
@@ -1,4 +1,5 @@
 const mockExec = jest.fn()
+const mockSendTransactionRpc = jest.fn()
 jest.mock('@nervosnetwork/ckb-sdk-rpc', () => {
   return class MockRpc {
     createBatchRequest() {
@@ -6,10 +7,11 @@ jest.mock('@nervosnetwork/ckb-sdk-rpc', () => {
         exec: mockExec,
       }
     }
+    sendTransaction = mockSendTransactionRpc
   }
 })
 
-import { checkPendingDeals } from './request'
+import { checkPendingDeals, signAndSendTransaction } from './request'
 
 describe('Test request', () => {
   describe('Test checkPendingOrders', () => {
@@ -38,6 +40,106 @@ describe('Test request', () => {
       it('should return all false', async () => {
         const res = await checkPendingDeals(MOCK_URL, MOCK_TX_HASHES)
         expect(res).toEqual([false, false, false])
+      })
+    })
+  })
+
+  describe('Test sign and send transaction', () => {
+    it('should send signed transaction', async () => {
+      const TX: CKBComponents.RawTransactionToSign = {
+        cellDeps: [],
+        headerDeps: [],
+        inputs: [
+          {
+            since: '0x0',
+            previousOutput: {
+              txHash: '0x001748c1b5749af9636b940ae7d8d56d12a3de83f9020e765a5e108ca87873fa',
+              index: '0x0',
+            },
+          },
+          {
+            since: '0x0',
+            previousOutput: {
+              txHash: '0x00317da41f64a75f7cb0bd0238857e1dc75d50fae3f8bbab1e20e18840df024c',
+              index: '0x0',
+            },
+          },
+        ],
+        outputs: [
+          {
+            capacity: '0x189640200',
+            lock: {
+              args: '0x3cb5366451256c3c9f8d4e2fc2e2aa8fa5774756',
+              codeHash: '0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8',
+              hashType: 'type',
+            },
+          },
+          {
+            capacity: '0x31eb37587',
+            lock: {
+              args: '0xc11d75be4531c9c0cca7531bab077f9a1710c87c',
+              codeHash: '0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8',
+              hashType: 'type',
+            },
+          },
+        ],
+        outputsData: ['0x', '0x'],
+        version: '0x0',
+        witnesses: [
+          {
+            lock: '',
+            inputType: '',
+            outputType: '',
+          },
+          '0x',
+        ],
+      }
+
+      const PRIV_KEY = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
+      await signAndSendTransaction(TX, PRIV_KEY)
+      expect(mockSendTransactionRpc).toBeCalledWith({
+        cellDeps: [],
+        headerDeps: [],
+        inputs: [
+          {
+            previousOutput: {
+              index: '0x0',
+              txHash: '0x001748c1b5749af9636b940ae7d8d56d12a3de83f9020e765a5e108ca87873fa',
+            },
+            since: '0x0',
+          },
+          {
+            previousOutput: {
+              index: '0x0',
+              txHash: '0x00317da41f64a75f7cb0bd0238857e1dc75d50fae3f8bbab1e20e18840df024c',
+            },
+            since: '0x0',
+          },
+        ],
+        outputs: [
+          {
+            capacity: '0x189640200',
+            lock: {
+              args: '0x3cb5366451256c3c9f8d4e2fc2e2aa8fa5774756',
+              codeHash: '0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8',
+              hashType: 'type',
+            },
+          },
+          {
+            capacity: '0x31eb37587',
+            lock: {
+              args: '0xc11d75be4531c9c0cca7531bab077f9a1710c87c',
+              codeHash: '0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8',
+              hashType: 'type',
+            },
+          },
+        ],
+        outputsData: ['0x', '0x'],
+        version: '0x0',
+        witnesses: [
+          '0x5500000010000000550000005500000041000000b112acb3f3730e490122e5cb6175edea3049cc87789666aefa2f84f47b64e78021921509e2b4244e3a1f8f1e898a9b95b2ea514f880ef8b4587fcb4066bef5f100',
+          '0x',
+        ],
       })
     })
   })
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,6 +1,6 @@
 import Rpc from '@nervosnetwork/ckb-sdk-rpc'
 import CKB from '@nervosnetwork/ckb-sdk-core'
-import { DEFAULT_NODE_URL, ORDER_SCRIPTS } from '../utils'
+import { DEFAULT_NODE_URL } from '../utils'
 
 export const checkPendingDeals = async (rpcUrl: string, txHashes: string[]) => {
   const rpc = new Rpc(rpcUrl)
@@ -17,29 +17,12 @@ export const checkPendingDeals = async (rpcUrl: string, txHashes: string[]) => {
 export const signAndSendTransaction = async (
   rawTransaction: CKBComponents.RawTransactionToSign,
   privateKey: string,
-  lock: CKBComponents.Script,
 ) => {
   const ckb = new CKB(DEFAULT_NODE_URL)
-  const keys = new Map()
-  keys.set(ckb.utils.scriptToHash(lock), privateKey)
-  const signedWitnesses = ckb.signWitnesses(keys)({
+  const signedWitness = ckb.signWitnesses(privateKey)({
     transactionHash: ckb.utils.rawTransactionToHash(rawTransaction),
-    witnesses: rawTransaction.witnesses,
-    inputCells: rawTransaction.inputs.map((input, index) => {
-      return {
-        outPoint: input.previousOutput,
-        lock:
-          index === 0
-            ? lock
-            : {
-                args: rawTransaction.outputs[1].lock.args,
-                codeHash: ORDER_SCRIPTS.lock.code_hash,
-                hashType: ORDER_SCRIPTS.lock.hash_type,
-              },
-      }
-    }),
-    skipMissingKeys: true,
-  })
-  const signedTx: any = { ...rawTransaction, witnesses: signedWitnesses }
+    witnesses: [{ lock: '', inputType: '', outputType: '' }],
+  })[0]
+  const signedTx: any = { ...rawTransaction, witnesses: [signedWitness, ...rawTransaction.witnesses.slice(1)] }
   return await ckb.rpc.sendTransaction(signedTx)
 }
